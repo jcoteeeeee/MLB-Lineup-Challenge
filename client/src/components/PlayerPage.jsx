0bab4abfc0c874e665d2968b46c84731cbb8cc4e
John Cote <johncote61598@gmail.com>
2023-01-23 15:24:59 -0500

    created playerpage, teampage and signup components

--- client/src/components/PlayerPage.jsx
+++ client/src/components/PlayerPage.jsx
@@ -2,11 +2,41 @@ const PlayerPage = () => {
     return(
         <div>
             <div id='player-list'>
-                
+
                 <div id='pitchers' className='position'>
                     
+                </div> 
+
+                <div id='catchers' className='position'>
+
+                </div> 
+
+                <div id='first-basemen' className='position'>
+
+                </div> 
+
+                <div id='second-basemen' className='position'>
+
+                </div> 
+
+                <div id='shortstops' className='position'>
+
+                </div> 
+
+                <div id='left-fielders' className='position'>
+
+                </div> 
+
+
+                <div id='center-fielders' className='position'>
+
                 </div>
 
+
+                <div id='right-fielders' className='position'>
+
+                </div>
+                
             </div>
         </div>
     )
---
+++ client/src/components/TeamPage.jsx
@@ -0,0 +1,9 @@
+const TeamPage = () => {
+    return(
+        <div>
+
+        </div>
+    )
+}
+
+export default TeamPage  
