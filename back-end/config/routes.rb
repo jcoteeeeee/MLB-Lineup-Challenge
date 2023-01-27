185571a732e4f2b3007b4d3f7f8e8b8413768f38
John Cote <johncote61598@gmail.com>
2023-01-27 09:03:07 -0500

    added new routes and actions

--- back-end/config/routes.rb
+++ back-end/config/routes.rb
@@ -1,10 +1,14 @@
 Rails.application.routes.draw do
   get 'users', to: 'users#index'
   get 'users/:id', to: 'users#show'
+  post 'users', to: 'users#create'
+  delete 'users/:id', to: 'users#destroy'
 
   get 'players', to: 'players#index'
   get 'players/:id', to: 'players#show'
 
   get 'teams', to: 'teams#index'
   get 'teams/:id', to: 'teams#show'
+  post 'teams', to: 'teams#create'
+  delete 'teams/:id', to: 'teams#destroy'
 end
